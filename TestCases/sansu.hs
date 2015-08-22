82c12e4d2a19f90bf5f932e480737a44dce0b5b8
WooKyoung Noh <wookay.noh@gmail.com>
2015-08-22 14:22:45 +0200

    travis

--- TestCases/sansu.hs
+++ TestCases/sansu.hs
@@ -1,19 +1,11 @@
-main =
-  let
+일을더해 = (+) 1
+일을더해 :: Int -> Int
 
-    일을더해 = (+) 1
-    일을더해 :: Int -> Int
+삼을곱해 = (*) 3
+삼을곱해 :: Int -> Int
 
-    삼을곱해 = (*) 3
-    삼을곱해 :: Int -> Int
+이에다가 f = f 2
+이에다가 :: (Int -> Int) -> Int
 
-    이에다가 f = f 2
-    이에다가 :: (Int -> Int) -> Int
-
-  in do
-
-    print( 일을더해 2        ) -- 3
-    print( 삼을곱해 2        ) -- 6
-
-    print( 이에다가 일을더해 ) -- 3
-    print( 이에다가 삼을곱해 ) -- 6
+main = do
+  print()
